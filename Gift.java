6c4f2703924db6eed845185e5f459d723c16f2e1
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-09-17 18:24:08 -0300

    Adicionei o projeto presente para minha namorada.

---
+++ Gift.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Oi amor, fiz esse programa para ti, vou falar aqui sobre o teu"
+		+ " caminho na programacao junto comingo.");
+		
+		System.out.print("Digite love para continuar: ");
+		in.next();
+		
+		System.out.println("Amor, o comeco foi bem aleatorio, no entanto acho que a magia da coisa foi essa.");
+		System.out.println("Enfim, tudo comecou no Scratch, foi uma parte dificil para ti, pelo menos no comeco,"  
+		+ "\nera um coisa completamente nova e tu persistiu e chegou ate aqui! \nMe sinto muito orgulhoso de ti, muito mesmo.");
+		
+		System.out.print("Digite love para continuar: ");
+		in.next();
+		
+		in.close();
+	}
+}
